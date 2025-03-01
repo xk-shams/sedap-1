83c63e77a5d57771d98a2d5fbe61c0f033884d63
anvarov2295 <anvarov2295@gmail.com>
2025-03-01 18:00:45 +0500

    some changes

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -1,52 +1,38 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function Navigation(props) {
+  const router = useRouter();
+  console.log("router", router.asPath);
   return (
     <div>
       <div
         style={{
-            backgroundColor: 'aliceblue',
-            display: 'flex',
-            flexDirection: 'column'
+          backgroundColor: "aliceblue",
+          display: "flex",
+          flexDirection: "column",
         }}
       >
-        <Link href="/">
-          Dashboard
-        </Link>
-        <Link href="/orders">
-          Order List
-        </Link>
-        <Link href="#">
-          Order Detail
-        </Link>
-        <Link href="/customers">
+        <Link href="/">Dashboard</Link>
+        <Link href="/orders">Order List</Link>
+        <Link href="#">Order Detail</Link>
+        <Link
+          style={{
+            background: router.asPath === "/customers" ? "red" : undefined,
+          }}
+          href="/customers"
+        >
           Customer
         </Link>
-        <Link href="#">
-          Analytics
-        </Link>
-        <Link href="#">
-          Reviews
-        </Link>
-        <Link href="#">
-          Foods
-        </Link>
-        <Link href="#">
-          Food Detail
-        </Link>
-        <Link href="#">
-          Customer Detail
-        </Link>
-        <Link href="#">
-          Calendar
-        </Link>
-        <Link href="#">
-          Chat
-        </Link>
-        <Link href="#">
-          Wallet
-        </Link>
+        <Link href="#">Analytics</Link>
+        <Link href="#">Reviews</Link>
+        <Link href="#">Foods</Link>
+        <Link href="#">Food Detail</Link>
+        <Link href="#">Customer Detail</Link>
+        <Link href="#">Calendar</Link>
+        <Link href="#">Chat</Link>
+        <Link href="#">Wallet</Link>
       </div>
     </div>
   );
